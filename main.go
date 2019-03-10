4b09ff74c090d2d2eda2ede98c31bcc924865880
Irainia <anwarhidayat14@gmail.com>
2019-03-10 22:24:33 +0700

    adding bussiness logic in main

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/Irainia/tdd_golang/human"
+)
+
+func main() {
+	person := human.NewPerson()
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("guess: ")
+		inputAgeString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		inputAgeString = strings.Replace(inputAgeString, "\n", "", -1)
+
+		inputAgeInt, err := strconv.Atoi(inputAgeString)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = person.GuessAge(inputAgeInt)
+		if err == nil {
+			fmt.Println("correct")
+			break
+		}
+
+		fmt.Println(err.Error())
+	}
 }
